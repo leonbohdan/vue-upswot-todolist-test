dccead7f6d2eaeb886670e21e192c75960689ad5
Bohdan Leonets <leonetsbogdan@gmail.com>
2022-03-12 23:16:04 +0200

    add new todo component was created and was imported on todo list page

---
+++ src/components/AddTodoForm.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="flex f-column mb-40 p-rel">
+      <label for="todoName" class="mb-5">Todo Name</label>
+      <input type="text" name="todoName" v-model="newTodo.name" />
+    </div>
+
+    <div class="flex f-column mb-40 p-rel">
+      <label for="todoBody" class="mb-5">Description</label>
+      <input type="text" name="todoBody" v-model="newTodo.body" />
+    </div>
+
+    <div class="text-c">
+      <button type="button" class="button boxed" @click="saveTodo()">
+        Save Todo
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddTodoForm",
+  data() {
+    return {
+      newTodo: {
+        name: "",
+        body: "",
+      },
+    };
+  },
+  methods: {
+    saveTodo() {
+      console.log("saveTodo");
+      this.$emit("close-add-form");
+    },
+  },
+};
+</script>
+
+<style lang="sass"></style>
--- src/views/TodoListPage.vue
+++ src/views/TodoListPage.vue
@@ -45,9 +45,49 @@
       <div class="todosBox">
         <div class="size60 bold text-c">Thank you {{ getUser.username }}</div>
 
-        <div v-for="(todo, i) in getTodos" :key="i" class="mb-20">
-          <div class="bold-500">
+        <div v-if="!isAddTodoForm" class="text-c mb-20">
+          <button type="button" class="button boxed" @click="addTodo()">
+            Add Todo
+          </button>
+        </div>
+
+        <add-todo-form v-if="isAddTodoForm" @close-add-form="closeAddForm()" />
+
+        <div v-else v-for="(todo, i) in getTodos" :key="i" class="todo mb-20">
+          <div class="bold-500 flex f-center">
             {{ todo.name }}
+
+            <svg
+              @click="editTodo()"
+              class="pointer ml-10 svgTodo"
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              width="20"
+              height="20"
+              viewBox="0 0 30 30"
+              style="fill: #000000"
+            >
+              <path
+                d="M 22.828125 3 C 22.316375 3 21.804562 3.1954375 21.414062 3.5859375 L 19 6 L 24 11 L 26.414062 8.5859375 C 27.195062 7.8049375 27.195062 6.5388125 26.414062 5.7578125 L 24.242188 3.5859375 C 23.851688 3.1954375 23.339875 3 22.828125 3 z M 17 8 L 5.2597656 19.740234 C 5.2597656 19.740234 6.1775313 19.658 6.5195312 20 C 6.8615312 20.342 6.58 22.58 7 23 C 7.42 23.42 9.6438906 23.124359 9.9628906 23.443359 C 10.281891 23.762359 10.259766 24.740234 10.259766 24.740234 L 22 13 L 17 8 z M 4 23 L 3.0566406 25.671875 A 1 1 0 0 0 3 26 A 1 1 0 0 0 4 27 A 1 1 0 0 0 4.328125 26.943359 A 1 1 0 0 0 4.3378906 26.939453 L 4.3632812 26.931641 A 1 1 0 0 0 4.3691406 26.927734 L 7 26 L 5.5 24.5 L 4 23 z"
+              ></path>
+            </svg>
+
+            <svg
+              @click="deleteTodo()"
+              class="pointer ml-10 svgTodo"
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              width="20"
+              height="20"
+              viewBox="0 0 30 30"
+              style="fill: #000000"
+            >
+              <path
+                d="M 14.984375 2.4863281 A 1.0001 1.0001 0 0 0 14 3.5 L 14 4 L 8.5 4 A 1.0001 1.0001 0 0 0 7.4863281 5 L 6 5 A 1.0001 1.0001 0 1 0 6 7 L 24 7 A 1.0001 1.0001 0 1 0 24 5 L 22.513672 5 A 1.0001 1.0001 0 0 0 21.5 4 L 16 4 L 16 3.5 A 1.0001 1.0001 0 0 0 14.984375 2.4863281 z M 6 9 L 7.7929688 24.234375 C 7.9109687 25.241375 8.7633438 26 9.7773438 26 L 20.222656 26 C 21.236656 26 22.088031 25.241375 22.207031 24.234375 L 24 9 L 6 9 z"
+              ></path>
+            </svg>
           </div>
 
           <div>{{ todo.body }}</div>
@@ -59,11 +99,35 @@
 
 <script>
 import { mapGetters } from "vuex";
+import AddTodoForm from "@/components/AddTodoForm.vue";
 
 export default {
   name: "TodoListPage",
+  components: {
+    AddTodoForm,
+  },
+  data() {
+    return {
+      isAddTodoForm: false,
+    };
+  },
   computed: {
     ...mapGetters(["getTodos", "getUser"]),
+  },
+  methods: {
+    deleteTodo() {
+      console.log("deleteTodo");
+    },
+    editTodo() {
+      console.log("editTodo");
+    },
+    addTodo() {
+      this.isAddTodoForm = true;
+      console.log("addTodo");
+    },
+    closeAddForm() {
+      this.isAddTodoForm = false;
+    },
   },
 };
 </script>
@@ -82,4 +146,8 @@ export default {
   background-color: #fff
   max-width: 1200px
   width: 100%
+
+.todo
+  .svgTodo
+    +hover(transform, scale(1.2))
 </style>
