06fe4255a69243d8041bd3ba89eb36b48a13369e
Bohdan Leonets <leonetsbogdan@gmail.com>
2022-03-13 16:28:15 +0200

    edit public path

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath:
-    process.env.NODE_ENV === "production" ? "/vue-upswot-todolist-test/" : "/",
+    process.env.NODE_ENV === "production"
+      ? "/vue-upswot-todolist-test/login"
+      : "/login",
 });
